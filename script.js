3ab646e125308fcf8d616cafebd5044fa2508157
EdwardCraft325 <250i0079@martineztorre.tecnm.mx>
2025-11-26 12:21:49 -0600

    Update script.js

--- script.js
+++ script.js
@@ -6,21 +6,38 @@ function desdeDecimal(dec) {
 
 function desdeOctal(oct) {
     let dec = parseInt(oct, 8);
-    if (isNaN(dec)) return; //Evita errores si el valor no es válido
-    document.getElementById("decimal").value = dec;
     desdeDecimal(dec);
+    document.getElementById("decimal").value = dec;
 }
 
 function desdeHex(hex) {
     let dec = parseInt(hex, 16);
-    if (isNaN(dec)) return; //Evita errores si el valor no es válido{
-    document.getElementById("decimal").value = dec;
     desdeDecimal(dec);
+    document.getElementById("decimal").value = dec;
 }
 
 function desdeBin(bin) {
     let dec = parseInt(bin, 2);
-    if (isNaN(dec)) return; //Evita errores si el valor no es válido{
-    document.getElementById("decimal").value = dec;
     desdeDecimal(dec);
+    document.getElementById("decimal").value = dec;
 }
+
+document.getElementById("decimal").addEventListener("input", e => {
+    let val = e.target.value;
+    if (val !== "") desdeDecimal(parseInt(val));
+});
+
+document.getElementById("octal").addEventListener("input", e => {
+    let val = e.target.value;
+    if (val !== "") desdeOctal(val);
+});
+
+document.getElementById("hex").addEventListener("input", e => {
+    let val = e.target.value;
+    if (val !== "") desdeHex(val);
+});
+
+document.getElementById("binario").addEventListener("input", e => {
+    let val = e.target.value;
+    if (val !== "") desdeBin(val);
+});
